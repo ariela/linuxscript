c192cbad6bbeda55d27fc9ebee60b560c53dcbe0
Takeshi Kawamoto <yuki@transrain.net>
2011-05-27 15:55:27 +0900

    dotfileのインストールスクリプト作成 screenの設定変更

---
+++ dotfile_install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd ~/
+git clone https://github.com/ariela/linuxscript.git
+ln -sf ~/linuxscript/dotfiles/.zshrc .zshrc
+ln -sf ~/linuxscript/dotfiles/.screenrc .screenrc
+ln -sf ~/linuxscript/dotfiles/.vimrc .vimrc
