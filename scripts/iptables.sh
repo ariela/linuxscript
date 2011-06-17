d622b9487f3fd6951bee0b8363cccc9e44892688
root <root@www1060ua.sakura.ne.jp>
2011-06-17 10:20:21 +0900

    changes scripts.

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -1,5 +1,5 @@
-#!/bin.sh
-
+#!/bin/sh
+ 
 # iptablesの設定初期化
 /sbin/iptables -F
 /sbin/iptables -X
@@ -8,11 +8,19 @@
 /sbin/iptables -P INPUT DROP
 /sbin/iptables -P OUTPUT ACCEPT
 /sbin/iptables -P FORWARD DROP
- 
-# ローカルネットワーク設定
+
+# ローカルホストは受け付ける 
 /sbin/iptables -A INPUT -i lo -j ACCEPT
+/sbin/iptables -A OUTPUT -o lo -j ACCEPT
+
+# ステートフルインスペクションを有効化 
 /sbin/iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# 同一ネットワークからの接続を弾く(外部設置サーバ用)
+/sbin/iptables -A INPUT -s 10.0.0.0/8 -j DROP
+/sbin/iptables -A INPUT -s 172.16.0.0/12 -j DROP
+/sbin/iptables -A INPUT -s 192.168.0.0/16 -j DROP
+ 
 # フラグメントをドロップ
 /sbin/iptables -A INPUT -f -j LOG --log-level debug --log-prefix 'FRAGMENT DROP:'
 /sbin/iptables -A INPUT -f -j DROP
@@ -20,53 +28,39 @@
 # PINGを許可(1分に10回のみ解答)
 /sbin/iptables -A INPUT -p icmp --icmp-type echo-request -m hashlimit --hashlimit-name t_icmp --hashlimit 1/m --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-htable-expire 120000 -j ACCEPT
 
+# IDENTをドロップ
+/sbin/iptables -A INPUT -p tcp --dport 113 -j REJECT --reject-with tcp-reset
+
 # マルチキャストをドロップ
 /sbin/iptables -A INPUT -d 255.255.255.255 -j DROP
 /sbin/iptables -A INPUT -d 224.0.0.1 -j DROP
 
-# IDENTをドロップ
-/sbin/iptables -A INPUT -p tcp --dport 113 -j REJECT --reject-with tcp-reset
+# FTPを許可（5分に5回のアクセス失敗時はブロック）
+/sbin/iptables -A INPUT -p tcp --dport 20 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --syn --dport 20 -m recent --name ftpattack --set
+/sbin/iptables -A INPUT -p tcp --syn --dport 20 -m recent --name ftpattack --rcheck --seconds 300 --hitcount 5 -j LOG --log-prefix 'FTP attack'
+/sbin/iptables -A INPUT -p tcp --syn --dport 20 -m recent --name ftpattack --rcheck --seconds 300 --hitcount 5 -j DROP
 
-# sshを許可(1分に1回のみ接続)
-/sbin/iptables -A INPUT -p tcp -m state --syn --state NEW --dport 22 -m hashlimit --hashlimit-name t_sshd --hashlimit 1/m --hashlimit-burst 1 --hashlimit-mode srcip --hashlimit-htable-expire 120000 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --syn --dport 21 -m recent --name ftpattack --set
+/sbin/iptables -A INPUT -p tcp --syn --dport 21 -m recent --name ftpattack --rcheck --seconds 300 --hitcount 5 -j LOG --log-prefix 'FTP attack'
+/sbin/iptables -A INPUT -p tcp --syn --dport 21 -m recent --name ftpattack --rcheck --seconds 300 --hitcount 5 -j DROP
 
-# http/httpsを許可
+# SSHを許可（5分に5回のアクセス失敗時はブロック）
+/sbin/iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --syn --dport 22 -m recent --name sshattack --set
+/sbin/iptables -A INPUT -p tcp --syn --dport 22 -m recent --name sshattack --rcheck --seconds 300 --hitcount 5 -j LOG --log-prefix 'SSH attack'
+/sbin/iptables -A INPUT -p tcp --syn --dport 22 -m recent --name sshattack --rcheck --seconds 300 --hitcount 5 -j DROP
+
+# HTTP/HTTPS設定
 /sbin/iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-/sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+#/sbin/iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
-# ftpを許可
-# /sbin/iptables -A INPUT -p tcp --dport 21 -j ACCEPT
-# /sbin/iptables -A INPUT -p tcp --dport 4000:4029 -j ACCEPT
-
-# DNSを許可
-#/sbin/iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-#/sbin/iptables -A INPUT -p udp --dport 53 -j ACCEPT
-
-# SMTPを許可
+# メール設定
 #/sbin/iptables -A INPUT -p tcp --dport 25 -j ACCEPT
-
-# SMTPSを許可
-#/sbin/iptables -A INPUT -p tcp --dport 465 -j ACCEPT
-
-# POP3を許可
 #/sbin/iptables -A INPUT -p tcp --dport 110 -j ACCEPT
+#/sbin/iptables -A INPUT -p tcp --dport 143 -j ACCEPT
 
-# POP3Sを許可
-#/sbin/iptables -A INPUT -p tcp --dport 995 -j ACCEPT
-
-# IMAPを許可
-#iptables -A INPUT -p tcp --dport 143 -j ACCEPT
-
-# IMAPSを許可
-#/sbin/iptables -A INPUT -p tcp --dport 993 -j ACCEPT
-
-# ログ設定
-/sbin/iptables -A INPUT -m limit --limit 1/s -j LOG --log-level debug --log-prefix 'INPUT DROP:'
-/sbin/iptables -A INPUT -j DROP
-/sbin/iptables -A FORWARD -m limit --limit 1/s -j LOG --log-level debug --log-prefix 'FORWARD DROP:'
-/sbin/iptables -A FORWARD -j DROP
-
-# 再起動
-/sbin/service iptables save
+# iptables再起動
+/etc/rc.d/init.d/iptables save
 /sbin/service iptables restart
-/sbin/chkconfig iptables on
